0e49e6dbd66a2feac424c55fe04147b013834354
Tanya <tanya210300@gmail.com>
2026-02-21 12:34:13 +0200

    fix

--- app/main.py
+++ app/main.py
@@ -4,19 +4,13 @@ from app.people.cinema_staff import Cleaner
 from app.people.customer import Customer
 
 
-def cinema_visit(
-            movie: str,
-            customers: list,
-            hall_number: int,
-            cleaner: str
-            ) -> None:
-        customer_instances = [
-        Customer(name=customer["name"], food=customer["food"])
-        for customer in customers
+def cinema_visit(movie: str, customers: list, hall_number: int, cleaner: str) -> None:
+    customer_instances = [
+        Customer(name=customer["name"], food=customer["food"]) for customer in customers
     ]
 
-        for customer in customer_instances:
-            CinemaBar.sell_product(product=customer.food, customer=customer)
-        hall = CinemaHall(number=hall_number)
-        cleaning_staff = Cleaner(name=cleaner)
-        hall.movie_session(movie, customer_instances, cleaning_staff)
+    for customer in customer_instances:
+        CinemaBar.sell_product(product=customer.food, customer=customer)
+    hall = CinemaHall(number=hall_number)
+    cleaning_staff = Cleaner(name=cleaner)
+    hall.movie_session(movie, customer_instances, cleaning_staff)
